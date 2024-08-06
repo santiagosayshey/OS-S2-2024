7f10390257e52c0c9d66b91031898372c5cd8cb7
santiagosayshey <schau22@pm.me>
2024-08-06 23:33:49 +0930

    Implement enhanced minishell with background process support
    - Add support for running processes in background with '&'
    - Implement 'cd' command for directory navigation
    - Add error handling with perror for system calls
    - Fix child process termination on exec failure
    - Implement background process status reporting
    - Ensure full command with arguments is displayed in status messages
    - Remove unnecessary output for cleaner user experience
    - Add check for finished background processes before each prompt
    - Implement proper handling of multiple background processes

--- minishell.c
+++ minishell.c
@@ -1,13 +1,3 @@
-/*********************************************************************
-   Program  : miniShell                   Version    : 1.3
- --------------------------------------------------------------------
-   skeleton code for linix/unix/minix command line interpreter
- --------------------------------------------------------------------
-   File			: minishell.c
-   Compiler/System	: gcc/linux
-
-********************************************************************/
-
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -16,85 +6,118 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#define NV 20			/* max number of command tokens */
-#define NL 100			/* input buffer size */
-char            line[NL];	/* command input buffer */
+#define NV 20           /* max number of command tokens */
+#define NL 100          /* input buffer size */
+#define MAX_BG_PROCESSES 10
 
-int main(int argk, char *argv[], char *envp[])
-/* argk - number of arguments */
-/* argv - argument vector from command line */
-/* envp - environment pointer */
+char line[NL];  /* command input buffer */
 
-{
-  int             frkRtnVal;	/* value returned by fork sys call */
-  int             wpid;		/* value returned by wait */
-  char           *v[NV];	/* array of pointers to command line tokens */
-  char           *sep = " \t\n";/* command line token separators    */
-  int             i;		/* parse index */
+struct bg_process {
+    pid_t pid;
+    char command[NL];
+    int finished;
+};
 
+struct bg_process bg_processes[MAX_BG_PROCESSES];
+int bg_count = 0;
 
-  /* prompt for and process one command line at a time  */
-
-  while (1) {			/* do Forever */
-    if (fgets(line, NL, stdin) == NULL) {
-      if (feof(stdin)) {		/* non-zero on EOF  */
-        exit(0);
-      }
-    }
-    fflush(stdin);
-
-    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-      continue;			/* to prompt */
-
-    v[0] = strtok(line, sep);
-    for (i = 1; i < NV; i++) {
-      v[i] = strtok(NULL, sep);
-      if (v[i] == NULL)
-	break;
-    }
-    /* assert i is number of tokens + 1 */
-
-    // Handle cd command
-    if (v[0] && strcmp(v[0], "cd") == 0) {
-      if (v[1]) {
-        if (chdir(v[1]) != 0) {
-          perror("cd");
+void check_background_processes() {
+    for (int i = 0; i < bg_count; i++) {
+        if (bg_processes[i].pid != 0) {
+            int status;
+            pid_t result = waitpid(bg_processes[i].pid, &status, WNOHANG);
+            if (result == bg_processes[i].pid) {
+                printf("[%d]+ Done                    %s\n", i+1, bg_processes[i].command);
+                bg_processes[i].pid = 0;
+            } else if (result == -1) {
+                perror("waitpid");
+            }
         }
-      } else {
-        char *home = getenv("HOME");
-        if (home) {
-          if (chdir(home) != 0) {
-            perror("cd");
-          }
-        } else {
-          fprintf(stderr, "cd: HOME not set\n");
-        }
-      }
-      continue;  // Skip to next iteration after handling cd
     }
+}
 
-    /* fork a child process to exec the command in v[0] */
+int main() {
+    int frkRtnVal;  /* value returned by fork sys call */
+    int wpid;       /* value returned by wait */
+    char *v[NV];    /* array of pointers to command line tokens */
+    char *sep = " \t\n"; /* command line token separators */
+    int i;          /* parse index */
 
-    switch (frkRtnVal = fork()) {
-    case -1:			/* fork returns error to parent process */
-      {
-        perror("fork");
-	break;
-      }
-    case 0:			/* code executed only by child process */
-      {
-	execvp(v[0], v);
-	perror(v[0]);
-        exit(1);
-      }
-    default:			/* code executed only by parent process */
-    {
-        wpid = wait(NULL);
-        if (wpid == -1) {
-        perror("wait");
+    while (1) {     /* do Forever */
+        check_background_processes();
+        
+        // fprintf(stdout, "\n msh> ");  // Removed prompt
+        // fflush(stdout);
+
+        if (fgets(line, NL, stdin) == NULL) {
+            if (feof(stdin)) {
+                // fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
+                //         feof(stdin), ferror(stdin));
+                exit(0);
+            }
         }
-        break;
+
+        if (line[0] == '#' || line[0] == '\n' || line[0] == '\0')
+            continue;
+
+        v[0] = strtok(line, sep);
+        for (i = 1; i < NV; i++) {
+            v[i] = strtok(NULL, sep);
+            if (v[i] == NULL)
+                break;
+        }
+
+        if (v[0] == NULL) continue;
+
+        if (strcmp(v[0], "cd") == 0) {
+            if (v[1] == NULL) {
+                if (chdir(getenv("HOME")) != 0) {
+                    perror("chdir");
+                }
+            } else {
+                if (chdir(v[1]) != 0) {
+                    perror("chdir");
+                }
+            }
+            continue;
+        }
+
+        int is_background = 0;
+        if (i > 1 && strcmp(v[i-1], "&") == 0) {
+            is_background = 1;
+            v[i-1] = NULL;
+        }
+
+        switch (frkRtnVal = fork()) {
+            case -1:
+                perror("fork");
+                break;
+            case 0:
+                execvp(v[0], v);
+                perror("execvp");
+                exit(1);
+            default:
+                if (is_background) {
+                    bg_count++;
+                    printf("[%d] %d\n", bg_count, frkRtnVal);
+                    bg_processes[bg_count-1].pid = frkRtnVal;
+                    
+                    // Store the full command including arguments
+                    strcpy(bg_processes[bg_count-1].command, "");
+                    for (int j = 0; v[j] != NULL; j++) {
+                        strcat(bg_processes[bg_count-1].command, v[j]);
+                        if (v[j+1] != NULL) strcat(bg_processes[bg_count-1].command, " ");
+                    }
+                    
+                    bg_processes[bg_count-1].finished = 0;
+                } else {
+                    wpid = waitpid(frkRtnVal, NULL, 0);
+                    if (wpid == -1) {
+                        perror("waitpid");
+                    }
+                }
+                break;
+            }
     }
-    }				/* switch */
-  }				/* while */
-}				/* main */
+    return 0;
+}
